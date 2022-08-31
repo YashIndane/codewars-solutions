b12cd12a9d58acc5fc1dcb4e4e35dfadfbdc630b
Yash Indane <yashindane46@gmail.com>
2022-08-31 15:05:20 +0530

    Create Switch_it_Up!.py

---
+++ python/Switch_it_Up!.py
@@ -0,0 +1,4 @@
+def switch_it_up(number):
+    d = {0 : 'Zero' , 1 : 'One' , 2 : 'Two' , 3 : 'Three' ,4 : 'Four' , 5 : 'Five' , 6 : 'Six'
+        , 7 : 'Seven' , 8 : 'Eight' , 9 : 'Nine'}
+    return d[number]
