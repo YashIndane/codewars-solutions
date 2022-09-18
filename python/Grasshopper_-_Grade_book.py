9f8631746375e43ec633ffe7cddc75c1533232bc
Yash Indane <yashindane46@gmail.com>
2022-09-18 17:20:29 +0530

    Create Grasshopper_-_Grade_book.py

---
+++ python/Grasshopper_-_Grade_book.py
@@ -0,0 +1,13 @@
+def get_grade(s1, s2, s3):
+    # Code here
+    score = (s1+s2+s3)/3
+    if 90 <= score <= 100:
+        return 'A'
+    elif 80 <= score < 90:
+        return 'B'
+    elif 70 <= score < 80:
+        return 'C'
+    elif 60 <= score < 70:
+        return 'D'
+    else:
+        return 'F'
