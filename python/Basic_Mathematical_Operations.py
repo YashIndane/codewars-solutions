1a22b12ab89cf6a0d9615ea015c10e7a6e8ec92e
Yash Indane <yashindane46@gmail.com>
2022-12-19 16:33:08 +0530

    Create Basic_Mathematical_Operations.py

---
+++ python/Basic_Mathematical_Operations.py
@@ -0,0 +1,7 @@
+def basic_op(operator, a, b):
+    #your code here
+    if operator=="+": return a+b
+    elif operator=="*":  return a*b
+    elif operator=="-": return a-b
+    else:
+        return a/b
